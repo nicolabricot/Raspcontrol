9e417a98b00ebb503a659c6ec7a3c3f9b18a24ba
Nicolas Devenet <github@devenet.info>
2014-04-13 16:28:53 +0200

    Add assets files
    * Bootstrap
    * jQuery
    * Favicons

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Raspcontrol</title>
+
+		<meta name="author" content="Nicolas Devenet" />
+		<meta name="robots" content="noindex, nofollow, noarchive" />
+		<meta name="description" content="The Raspberry Pi Control Center" />
+		
+		<link rel="shortcut icon" href="assets/icons/favicon.ico" />
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/icons/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/icons/apple-touch-icon-60x60.png" />
+		<link rel="icon" type="image/png" href="assets/icons/favicon-16x16.png" sizes="16x16" />
+		<link rel="icon" type="image/png" href="assets/icons/favicon-32x32.png" sizes="32x32" />
+		<meta name="msapplication-TileColor" content="#00aba9">
+		<meta name="msapplication-square70x70logo" content="assets/icons/mstile-70x70.png" />
+		<meta name="msapplication-square150x150logo" content="assets/icons/mstile-150x150.png" />
+		<meta name="msapplication-wide310x150logo" content="assets/icons/mstile-310x150.png" />
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="assets/css/raspcontrol.css" rel="stylesheet" media="screen" />
+
+		<!--[if lt IE 9]>
+			<script src="./assets/js/html5shiv.min.js"></script>
+			<script src="./assets/js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+
+		<div class="container">
+			<div class="page-header">
+			  <h1>Example page header <small>Subtext for header</small></h1>
+			</div>
+		</div>
+
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
